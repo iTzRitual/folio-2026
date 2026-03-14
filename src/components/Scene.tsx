1549017e9233bbfcfffe0bcd3c8c0f21c3e73cf6
Natan M <ritualhorse960@gmail.com>
2026-03-14 22:16:14 +0100

    ⚗️ Add temporary background

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,9 +1,24 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import Model from "./Model";
 import { GridOverlay } from "./HeroScene/GridOverlay";
-import { Environment } from "@react-three/drei";
+import { Environment, useTexture } from "@react-three/drei";
+
+function Background() {
+  const { viewport } = useThree();
+  const texture = useTexture("/gemini_bg.png");
+
+  return (
+    <mesh
+      scale={[viewport.width * 3, viewport.height * 3, 1]}
+      position={[0, 0, -3]}
+    >
+      <planeGeometry />
+      <meshBasicMaterial map={texture} depthTest={false} />
+    </mesh>
+  );
+}
 
 export default function Scene() {
   return (
@@ -11,6 +26,7 @@ export default function Scene() {
       <Canvas className="bg-[#1D1D1D]" key="main-canvas">
         <directionalLight intensity={3} position={[0, 3, 2]} />
         <Environment preset="city" />
+        <Background />
         <Model />
       </Canvas>
       {/* <GridOverlay /> */}
